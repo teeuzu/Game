f9636b85c3294218482593d83dafdacfa70b5ed3
Matheus Auret <matheus.cazemundo@g.globo>
2022-07-01 13:19:56 -0300

    Projeto Game em Python

---
+++ README.md
@@ -0,0 +1,16 @@
+## Descrição:
+<hr>
+
+Desenvolvendo um jogo, onde, ao ser inicializada, solicite ao usuário escolher o nível de dificuldade. E após isso gera
+e representa, de forma aleatória, um cálculo para que possamos informar o resultado.
+
+** As operações serão limitadas em: **
+
+1 - Soma;
+2 - Subtração;
+3 - Multiplicação;
+4 - divisão.
+
+
+Se o usuário acertar a resposta, somará um ponto ao seu score.
+Ë de sua opção continuar ou não no jogo, ganhando ou perdendo.
---
+++ game.py
@@ -0,0 +1,34 @@
+from models.calcular import Calcular
+
+def main() -> None:
+    pontos: int = 0
+    jogar(pontos)
+
+# Informando o nível de dificuldade do jogo
+def jogar(pontos: int) -> None:
+    dificuldade: int = int(input('Informe o nível de dificuldade desejado: [1, 2, 3 ou 4]: '))
+
+    # Recebendo a variável com o nível de dificuldade informado
+    calc: Calcular = Calcular(dificuldade)
+    print('Informe o resultado para a seguinte operação: ')
+    calc.mostrar_operacao() 
+
+    resultado: int = int(input())
+
+    if calc.checar_resultado(resultado):
+        pontos += 1
+        print(f'Você tem {pontos} pontos(s).\n')
+
+    continuar: int = int(input('Deseja continuar no jogo? [1 - sim, 0 - não]: '))
+
+    if continuar:
+        jogar(pontos)
+    else:
+        print(f'Você finalizou com {pontos} ponto(s).')
+        print('Até a próxima!')
+
+
+
+
+if __name__ == '__main__':
+    main()
